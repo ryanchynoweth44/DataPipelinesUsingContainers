304d6e4d9fa5e22a7d573c3d6139c0e3681959b9
Ryan Chynoweth <ryanchyno@outlook.com>
2019-01-02 15:13:42 -0800

    readme changes

--- README.md
+++ README.md
@@ -3,8 +3,19 @@ This repository shows an end to end implementation using containers to extract d
 
 ## Prerequisites
  - [Azure Subscription](https://azure.microsoft.com/en-us/free/search/?&OCID=AID719825_SEM_KX8R84uR&lnkd=Bing_Azure_Brand&msclkid=6e706d7f2c60158ed7103168c2415255&dclid=CNmloKvCp98CFVJgwQodwMcKKQ)
+    - You must have the subscription's Tenant Id, and [create an service principal with a client secret](https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-create-service-principal-portal). 
  - [Open Weather Map Free Account](https://openweathermap.org/appid)
- - Basic Knowledge of Python
+ - Basic knowledge and local installation of [Python](https://www.anaconda.com/download/)
+ - Python IDE. I use [Visual Studio Code]()
+ - Basic knowledge and local installation of [Docker]()
+
+
+## Running the project 
+The main part of this repository is the containerized code have written to extract weather data. The code is located in the "[code](./code)" folder path of this repository. If you wish to run the code you will need to do the following:
+1. Satisfy prerequisites
+1. Supply values in the [configuration file](code/application/app_config.conf). 
+1. Create docker image locally
+1. Run docker image
 
 ## Blog
 
--- code/application/extract_data.py
+++ code/application/extract_data.py
@@ -1,23 +1,28 @@
 import requests
 import os, sys
 import datetime
+import time
 
-#sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname('__file__'))) + '/code/application')
-from app_helper import AppHelper
+while True:
+    # reconfigure the app helper
+    from app_helper import AppHelper
+    appHelp = AppHelper()
+    adls_connect_obj = appHelp.connect_adls()
 
+    # set api query params
+    city_ids = ['5747882', '5809844', '5799841', '5816449', '5812944', '5786882']
+    city_batch = ",".join(city_ids)
+    cities = ['Redmond, WA, USA','Seattle, WA, USA', 'Kirkland, WA, USA', 'Woodinville, WA, USA', 'Tacoma, WA, USA', 'Bellevue, WA, USA']
 
-appHelp = AppHelper()
-adls_connect_obj = appHelp.connect_adls()
+    # get request to get weather info
+    r = requests.get("http://api.openweathermap.org/data/2.5/group?id=" + city_batch + "&appid=" + appHelp.weather_api_token)
 
+    # format output path by date
+    output_datetime = datetime.datetime.utcnow()
+    data_output_path = "raw/weather_data/{}/{}/{}/{}/{}/weather_data.json".format(output_datetime.year, output_datetime.month, output_datetime.day, output_datetime.hour, output_datetime.minute)
 
-city_ids = ['5747882', '5809844', '5799841', '5816449', '5812944', '5786882']
-city_batch = ",".join(city_ids)
-cities = ['Redmond, WA, USA','Seattle, WA, USA', 'Kirkland, WA, USA', 'Woodinville, WA, USA', 'Tacoma, WA, USA', 'Bellevue, WA, USA']
+    # write json file to adls
+    appHelp.write_json_file(adls_connect_obj, data_output_path, r.content.decode('utf8'))
 
-r = requests.get("http://api.openweathermap.org/data/2.5/group?id=" + city_batch + "&appid=" + appHelp.weather_api_token)
-
-output_datetime = datetime.datetime.utcnow()
-data_output_path = "raw/weather_data/{}/{}/{}/{}/{}".format(output_datetime.year, output_datetime.month, output_datetime.day, output_datetime.hour, output_datetime.minute)
-
-appHelp.write_json_file(adls_connect_obj, data_output_path, r.content.decode('utf8'))
-
+    # Open Weather Map gets new data about every 10 minutes. So we will sleep for 10. 
+    time.sleep(600)
